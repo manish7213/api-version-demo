d948ba481d8348a4cde224b56ffa300647ba19d9
Manish Kumar <manish.vickey93@gmail.com>
2021-12-13 20:24:26 +0530

    Initial commit

---
+++ src/main/java/com/example/apiversiondemo/controller/StudentController.java
@@ -0,0 +1,46 @@
+package com.example.apiversiondemo.controller;
+
+import com.example.apiversiondemo.model.Name;
+import com.example.apiversiondemo.model.StudentV1;
+import com.example.apiversiondemo.model.StudentV2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    // Method - I (static versioning)
+    @GetMapping(value = "/student/v1")
+    public StudentV1 staticV1() {
+        return new StudentV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/student/v2")
+    public StudentV2 staticV2() {
+        return new StudentV2(new Name("Bob", "Charlie"));
+    }
+
+    //Method - II (Passing version as request param)
+
+    @GetMapping(value = "/student/param", params = "version=1")
+    public StudentV1 paramV1() {
+        return new StudentV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/student/param", params = "version=2")
+    public StudentV2 paramV2() {
+        return new StudentV2(new Name("Bob", "Charlie"));
+    }
+
+    //Method - III (Passing version as Header)
+
+    @GetMapping(value = "/student/header", headers = "X-API-VERSION=1")
+    public StudentV1 headerV1() {
+        return new StudentV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/student/header", headers = "X-API-VERSION=2")
+    public StudentV2 headerV2() {
+        return new StudentV2(new Name("Bob", "Charlie"));
+    }
+}
---
+++ src/main/java/com/example/apiversiondemo/model/StudentV1.java
@@ -0,0 +1,15 @@
+package com.example.apiversiondemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class StudentV1 {
+
+    private String name;
+}
